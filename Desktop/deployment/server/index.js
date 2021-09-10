576b42e245b2080d80c565cb877ede8304d78c6c
bryan-beck <bdbeck89@gmail.com>
2021-09-10 07:36:24 -0700

    adding Lab

--- Desktop/deployment/server/index.js
+++ Desktop/deployment/server/index.js
@@ -2,13 +2,12 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
-
-app.get('/',function(req,res) {
-  res.sendFile(path.join(__dirname, '../index.html'));
-});
+SVGClipPathElement.length('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.html'))
+})
 
 const port = process.env.PORT || 4005
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
+    console.log('listening on port ${port}')
 })
